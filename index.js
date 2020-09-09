722f10648651f1d0ac18f55f0f4c84bfc15655b7
YassineAlo <m.yassine.aloui@gmail.com>
2020-09-09 19:47:59 +0200

    cleaning & comments

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const normalize = require('ffmpeg-normalize');
 const { promisify } = require('util');
 const fs = require('fs')
+// Enable async/await
 const normalizeAsync = promisify(normalize)
 
+// Gets files to be treated form "./input"
 const inputList = fs.readdirSync('./input')
-// console.log(inputList)
 
 const asyncNorm = async () => {
+
     for (i=0; i < inputList.length;i++) {
         console.log(inputList[i])
+        // Saves initial filename
         var filename = inputList[i]
+        // Change name to avoid ffmpeg command errors
         fs.renameSync('./input/'+ inputList[i], './input/input'+ i + '.mp4')
         await normalize({
             input: './input/input'+ i + '.mp4',
@@ -26,7 +30,8 @@ const asyncNorm = async () => {
             verbose: true
         })
         .then(normalized  => {
-            // Normalized
+            // Once Normalized
+            // Rename back to original name
             fs.renameSync('./output/input'+ i + '.mp4', './output/'+filename)
             console.log("Fini :  ", normalized)
         })
@@ -38,26 +43,3 @@ const asyncNorm = async () => {
 }
 
 asyncNorm()
-
-// normalize({
-//     input: "input4.mp4",
-//     output: 'out3.mp4',
-//     loudness: {
-//         normalization: 'ebuR128',
-//         target:
-//         {
-//             input_i: -23,
-//             input_lra: 7.0,
-//             input_tp: -2.0
-//         }
-//     },
-//     verbose: true
-// })
-// .then(normalized  => {
-//     // Normalized
-//     console.log("Fini :  ", normalized)
-// })
-// .catch(error => {
-//     // Some error happened
-//     console.log("Error :  ", error)
-// });
